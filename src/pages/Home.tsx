3dd46b45988ab3015447d53d2e534d48e2ebe3d6
tgbilski <130314190+tgbilski@users.noreply.github.com>
2025-09-21 23:43:15 -0400

    Update budget description in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -102,7 +102,7 @@ const HeroSectionContent = () => (
           </div>
           <CardTitle className="text-lg font-semibold text-gray-900 mb-2 group-hover:text-primary transition-colors uppercase">Step 2: Create a budget</CardTitle>
           <CardDescription className="text-sm text-gray-600 leading-relaxed">
-            Link your bank accounts to our secure app.
+            No need for bank accounts, type in your data.
             <br />
             Automatically track monthly expenses.
           </CardDescription>
